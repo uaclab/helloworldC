e75d0a598623f9b4a3533c4129e09a277688640d
UAC Lab <admin24@uaclab.net>
2024-08-09 11:50:31 +0200

    Hello world with name and age version

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 
 int main()
 {
-    printf("Hello world !");
+    int age;
+    char name[50];
+    printf("Enter your name ");
+    scanf("%s", name);
+    printf("Enter your age ");
+    scanf("%d", &age);
+    printf("Hello %s, you are %d old.", name, age);
     return (0);
 }
